bf7b1bd6073c0649e155a30f0096940c69623b01
blupetersilie <jonas.bluemler@outlook.de>
2022-11-21 19:01:41 +0100

    movement

---
+++ unity online game/Assets/scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float gravity = -10f;
+    [SerializeField] float jumpHeight = 2f;
+
+    Vector3 velocity;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+
+    [SerializeField] LayerMask groundMask;
+
+    bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
